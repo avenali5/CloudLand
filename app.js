4099454671d3cb6fb8ab7645cd8b247274263c0e
Alejo <git@github.com>
2021-12-31 11:44:46 -0300

    FC

---
+++ app.js
@@ -0,0 +1,9 @@
+
+let menu = document.querySelector('.menu')
+
+const openMenu = () => {
+    menu.style.transform = 'translateX(0)'
+}
+const closeMenu = () => {
+    menu.style.transform = 'translateX(-110%)'
+}
